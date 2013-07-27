248fc66eeebbbf97abaf46ae159e252e411d2ccf
ZaighumRajput <zaighum47@gmail.com>
2013-07-27 03:55:09 +0000

    Added Progress Bar

--- LearningNode/RWfileStream.js
+++ LearningNode/RWfileStream.js
@@ -2,11 +2,19 @@ var fs = require('fs'); // Require Filesystem module
 var http = require('http');
 
 http.createServer(function(request, response) {
+
     var newFile = fs.createWriteStream("UploadedX.md");
+    var fileBytes = request.headers['content-length'];
+    var uploadedBytes = 0;
+
     request.pipe(newFile);
 
-    request.on('end', function() {
-	response.end('Succesfully Uploaded');
-});
+    request.on('data', function(chunk) {
+	uploadedBytes += chunk.length;
+	var progress = (uploadedBytes / fileBytes)* 100;
+	response.write("progress: " + parseInt(progress, 10) + "%\n");
 
+});
+    request.on('end', function(){
+	response.end('Succesfully uploaded');});
 }).listen(8080);
