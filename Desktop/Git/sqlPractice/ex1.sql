03972aca6a58468e191340526c61f0581167e03a
Zaighum Rajput <zaighum47@gmail.com>
2014-07-11 14:23:43 +0200

    moved sqlPractice

--- Desktop/Git/sqlPractice/ex1.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE person (
-	id integer primary key,
-	first_name text,
-	last_name text,
-	age integer
-	money integer
-);
--- Desktop/Git/sqlPractice/ex5EC.sql
+++
@@ -1,4 +0,0 @@
-SELECT * FROM PET WHERE age > 10;
-SELECT * FROM person WHERE age > 23;
-SELECT * FROM person WHERE age < 23;
-SELECT * FROM person WHERE first_name = "Zaighum" AND age < 24;
